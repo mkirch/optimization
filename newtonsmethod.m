3cc29a765ba71471ee486136558d61d39acb220e
krchnr <krchnr@PBB-ITC-G05.iowa.uiowa.edu>
2016-04-10 22:56:02 -0500

    Finished

--- newtonsmethod.m
+++ newtonsmethod.m
@@ -1,23 +1,46 @@
-function [dk,xkplusone] = newtonsmethod(f,x0,max)
+function [xi,fxi,gxi] = newtonsmethod(f,x0,max,c1,tol)
 % f - function
 % x0 - initial vector starting point
+% max - maximum number of iterations
+% c1 - Armijo constant
+format long
 xi=x0;
-
-a=1;
 k=0;
-
 grad = gradient(f);
 hess = hessian(f);
 
 while k<max
     fxi = subs(f,transpose(symvar(f)),xi);
-    double(fxi)
     gxi = subs(grad,transpose(symvar(f)),xi);
     hxi = subs(hess,transpose(symvar(f)),xi);
-    dk = -inv(hxi)*gxi;
-    xplusone = xi+dk*a;
-    xi=xplusone;
-    k=k+1;
+    % If hess positive definite, use Newton's. 
+    % If hess not positive definite, use Steepest Descent.
+    [R,p] = chol(hxi);
+    if p==0
+        dk = -inv(hxi)*gxi;
+    else 
+        dk = -gxi;
+    end
+    
+    % Determine Step Length Using Armijo-Backtracking Algorithm
+    a = 1;
+    while (subs(f,transpose(symvar(f)),xi+a*dk)) > (fxi+c1*a*transpose(dk)*gxi)
+        a = a/2;
+    end
+    
+    % Next x
+    xk1 = xi+dk*a;
+    
+    % Stop or update
+    if abs(xi - xk1) < tol
+        xi = double(xk1);
+        fxi = double(subs(f,transpose(symvar(f)),xi));
+        gxi = double(subs(grad,transpose(symvar(f)),xi));
+        return            
+    else
+        xi = xk1; 
+        k=k+1;
+    end
 end
 end
 
--- scrip.m
+++ scrip.m
@@ -1,8 +1,8 @@
 syms x y;
 
-%Function and Inital X_0
+%Function and Initial X_0
 f = symfun(y^2-(x^2)*y - 3*y+x^4 + -x^3+2*x*y-x,[x y]);
-x0=[0;1];
+x0=[2;2];
 
-newtonsmethod(f,x0,5)
+[xi,fxi,gxi] = newtonsmethod(f,x0,50,10^-4,0.00000000001)
 
